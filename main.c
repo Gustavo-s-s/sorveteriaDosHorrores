b9433dbe07e498a9906053ca3e69efbf199e8a89
Gustavo S Silva <g.silva90@edu.pucrs.br>
2022-11-07 17:42:44 -0300

    finalizando agrupamento de cores/

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <math.h>
-#include <string.h>		// para usar strings
+#include <string.h> // para usar strings
 #include "include/SOIL.h"
 
 // Rotinas para acesso da OpenGL
@@ -8,7 +8,7 @@
 // Variáveis globais a serem utilizadas:
 typedef struct
 {
-    int quantidadeIgual;
+    int quantity;
     RGB8 cor;
 } Cell;
 
@@ -25,16 +25,16 @@ Image8 pic8;
 int modo;
 
 // Protótipos
-void load(char* name, Image24* pic);
+void load(char *name, Image24 *pic);
 void process();
 
-int calcDistance(RGB8 color, RGB8 comparator)
+double calcDistance(RGB8 color, RGB8 comparator)
 {
-    int squeredDeltaR = pow((color.r - comparator.r), 2);
-    int squeredDeltaG = pow((color.g - comparator.g), 2);
-    int squeredDeltaB = pow((color.b - comparator.b), 2);
+    double squeredDeltaR = pow((color.r - comparator.r), 2);
+    double squeredDeltaG = pow((color.g - comparator.g), 2);
+    double squeredDeltaB = pow((color.b - comparator.b), 2);
 
-    int cate = squeredDeltaR + squeredDeltaG + squeredDeltaB;
+    double cate = squeredDeltaR + squeredDeltaG + squeredDeltaB;
     return sqrt(cate);
 }
 
@@ -52,28 +52,25 @@ int findIndexOfLessDistance(Cell *source, int size, RGB8 target)
     return indexOflessDistance;
 }
 
-int containsCell(Cell *source, int size, RGB8 target)
+int indexOfCell(Cell *source, int size, RGB8 target)
 {
     for (int i = 0; i < size; i++)
     {
-        char r = source[i].cor.r;
-        char g = source[i].cor.g;
-        char b = source[i].cor.b;
-
-        if (target.r == r && target.g == g && target.b == b)
+        if (calcDistance(source[i].cor, target) < 30)
             return i;
     }
 
     return -1;
 }
 
-Cell *filter(Cell *source, int size, int quantidadeDeCores)
+// filtra cores mais populares
+Cell *filterByPopularity(Cell *source, int size, int quantidadeDeCores)
 {
     Cell *cells = malloc(sizeof(Cell) * quantidadeDeCores);
 
     for (int i = 0; i < size; i++)
     {
-        const int quant = source[i].quantidadeIgual;
+        const int quant = source[i].quantity;
 
         if (i < quantidadeDeCores)
         {
@@ -81,9 +78,14 @@ Cell *filter(Cell *source, int size, int quantidadeDeCores)
         }
         else
         {
-            int index = findIndexOfLessDistance(cells, quantidadeDeCores, source[i].cor);
+            int index = 0;
+            for (int j = 0; j < quantidadeDeCores; j++)
+            {
+                if (cells[index].quantity < cells[j].quantity)
+                    index = j;
+            }
 
-            if (quant > cells[index].quantidadeIgual)
+            if (quant > cells[index].quantity)
             {
                 cells[index] = source[i];
             }
@@ -92,8 +94,8 @@ Cell *filter(Cell *source, int size, int quantidadeDeCores)
 
     return cells;
 }
-
-Cell *findTheMostPopularCells(int quantyOfColors, RGB8 *colors, int size)
+// encontra e agrupa as cores mais populares
+Cell *groupBy(int quantyOfColors, RGB8 *colors, int size)
 {
     Cell *cells = malloc(sizeof(Cell) * (sizeX * sizeY));
     int sizeOfCells = 0;
@@ -103,41 +105,48 @@ Cell *findTheMostPopularCells(int quantyOfColors, RGB8 *colors, int size)
         if (i == 0)
         {
             cells[i].cor = colors[i];
-            cells[i].quantidadeIgual = 0;
+            cells[i].quantity = 0;
             sizeOfCells++;
             continue;
         }
 
-        int cellIndex = containsCell(cells, sizeOfCells, colors[i]);
+        int cellIndex = indexOfCell(cells, sizeOfCells, colors[i]);
         if (cellIndex < 0)
         {
             cells[sizeOfCells].cor = colors[i];
-            cells[sizeOfCells].quantidadeIgual = 0;
+            cells[sizeOfCells].quantity = 0;
             sizeOfCells++;
         }
         else
         {
-            cells[cellIndex].quantidadeIgual += 1;
+            cells[cellIndex].quantity += 1;
         }
     }
 
-    return filter(cells, sizeOfCells, quantyOfColors);
+    return filterByPopularity(cells, sizeOfCells, quantyOfColors);
 }
 
 void loadPall(Cell *colors, int size)
 {
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++) 
         pic8.pal[i] = colors[i].cor;
+    
+}
+
+void generateNewImage(Cell *cells, int quantidadeDeCores)
+{
+    for (int i = 0; i < sizeX * sizeY; i++)
+        pic8.pixels[i] = findIndexOfLessDistance(cells, quantidadeDeCores, pic.pixels[i]);
 }
 
 // Carrega uma imagem para a struct Img
-void load(char* name, Image24* pic)
+void load(char *name, Image24 *pic)
 {
     int chan;
-    pic->pixels = (RGB8*) SOIL_load_image(name, &pic->width, &pic->height, &chan, SOIL_LOAD_RGB);
-    if(!pic->pixels)
+    pic->pixels = (RGB8 *)SOIL_load_image(name, &pic->width, &pic->height, &chan, SOIL_LOAD_RGB);
+    if (!pic->pixels)
     {
-        printf( "SOIL loading error: '%s'\n", SOIL_last_result() );
+        printf("SOIL loading error: '%s'\n", SOIL_last_result());
         exit(1);
     }
     printf("Load: %d x %d x %d\n", pic->width, pic->height, chan);
@@ -154,40 +163,36 @@ void process()
     // SUBSTITUA este código pelos algoritmos a serem implementados
     //
     int quantidadeDeCores = 256;
-    Cell *cells = findTheMostPopularCells(quantidadeDeCores, pic.pixels, sizeX * sizeY);
+    Cell *cells = groupBy(quantidadeDeCores, pic.pixels, sizeX * sizeY);
     loadPall(cells, quantidadeDeCores);
+    generateNewImage(cells, quantidadeDeCores);
 
-    for (int i = 0; i < sizeX * sizeY; i++)
-    {
-        pic8.pixels[i] = findIndexOfLessDistance(cells, quantidadeDeCores, pic.pixels[i]);
-    }
     // Exemplo: imagem de 8 bits (com outras cores, para testar)
- 
-
     //
     // NÃO ALTERAR A PARTIR DAQUI!!!!
     //
     buildTex();
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    if(argc==1) {
+    if (argc == 1)
+    {
         printf("colors [image file]\n");
         exit(1);
     }
 
     // Inicialização da janela gráfica
-    init(argc,argv);
+    init(argc, argv);
 
     load(argv[1], &pic);
     sizeX = pic.width;
     sizeY = pic.height;
 
     // Aloca memória para imagem de 8 bits
-    pic8.width  = sizeX;
+    pic8.width = sizeX;
     pic8.height = sizeY;
-    pic8.pixels = (unsigned char*) malloc(sizeX * sizeY);
+    pic8.pixels = (unsigned char *)malloc(sizeX * sizeY);
 
     modo = IMG24;
 
